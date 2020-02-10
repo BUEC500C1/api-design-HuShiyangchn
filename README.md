0bae38e0d850d3b80dd3d534f6a362663852e6db
hushiyangch <ancient_bamboo@163.com>
2020-02-09 21:40:00 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # api-design-HuShiyangchn
+## Introduction
+In this module, you can get the brief introduction about the airport and the current weather information about the airport which is based on Openweathermap API.
+## Implementation
+First I generated a file inlcuded all information about US airport based on airport-codes.csv which is produced by @svetozarstojkovic. More information about the file can be found in https://github.com/datasets/airport-codes. Then the program could provide short introduction about the airport and find the precise coordinates based on this file. Then the program can use the coordinates to call the API and get the precise weather information.
+## Example
+Run the program by type ```python3 weatherapi.py```
+After running you should enter the name of the airport:
+
+Then you can see the brief introduction and the weather information of the airport:
+
--- weatherapi.py
+++ weatherapi.py
@@ -1,39 +1,43 @@
 import requests
 import csv
 
-airportname=input("Please input the airport you are searching for:")
+airportname=input("Please enter the full name of the airport you are searching for:")
 filename='us-airport-codes.csv'
 urllist=["http://api.openweathermap.org/data/2.5/weather?lat="]
 colist=[]
+count=0
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     for row in reader:
+        count=count+1
         if airportname==row[2]:
             coordinates=row[11]
             print("The {}".format(row[2]),"is a {}".format(row[1]),"which is belongs to {}.".format(row[7]),end=" ")
             costr=row[11]
-        else:
-            print("No such airport was found, please enter the full name of airport.")
-costr.lstrip("'")
-costr.rstrip("'")
-coli=costr.split(",")
-lon=coli[1]
-lat=str(int(float(coli[0])))
-lon=str(int(float(lon.lstrip())))
-urllist.append(lat)
-urllist.append('&lon=')
-urllist.append(lon)
-#add your openweather key here 
-urllist.append('&APPID=your own key')
-url="".join(urllist)
-r = requests.get(url)
-response_dict = r.json()
-#print(response_dict)
-weather_dic=response_dict['weather']
-weather_info=weather_dic[0]
-wind_dic=response_dict['wind']
-main_dic=response_dict['main']
-print("The current weather of the airport is {},".format(weather_info['description']),end=" ")
-print(", and the temperature is {}".format(main_dic['temp']),"The pressure is {}".format(main_dic['pressure']),"meanwhile the humidity is{}".format(main_dic['humidity']),end=" ")
-print("In addition the wind speed is {}".format(wind_dic['speed']),"with {}".format(wind_dic['deg']),"degrees")
+            count=0       
+#if count==22875:
+    #print("No such airport was found, please enter the full name of airport.")
+#elif count<22875:
+    costr.lstrip("'")
+    costr.rstrip("'")
+    coli=costr.split(",")
+    lon=coli[1]
+    lat=str(int(float(coli[0])))
+    lon=str(int(float(lon.lstrip())))
+    urllist.append(lat)
+    urllist.append('&lon=')
+    urllist.append(lon)
+    #add your openweather key here 
+    urllist.append('&APPID=0a129340c5fe92e70a136ea7b21382d5')
+    url="".join(urllist)
+    r = requests.get(url)
+    response_dict = r.json()
+    #print(response_dict)
+    weather_dic=response_dict['weather']
+    weather_info=weather_dic[0]
+    wind_dic=response_dict['wind']
+    main_dic=response_dict['main']
+    print("The current weather of the airport is {},".format(weather_info['description']),end=" ")
+    print("and the temperature is {}".format(main_dic['temp']),"The pressure is {}".format(main_dic['pressure']),"meanwhile the humidity is{}".format(main_dic['humidity']),end=" ")
+    print("In addition the wind speed is {}".format(wind_dic['speed']),"with {}".format(wind_dic['deg']),"degrees")
