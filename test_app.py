5503aae1d9e748248f24c2c6903451be7007178c
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-05-09 13:07:17 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,5 +3,5 @@ import tornado.web
 from weatherapi import weather
 
 
-def test_case1(Williams Ag Airport):
+def test_case1(Williams%20Ag%20Airport):
     assert weather("00AK") == "The Williams Ag Airport is a small_airport, which is belongs to Biggs. The current weather of the airport is clear sky, and the temperature is 299.33. The pressure is 1014 meanwhile the humidity is 31. In addition, the wind speed is 1.49 with 253 degrees."
