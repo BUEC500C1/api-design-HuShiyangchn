bab706a3012face5dae38eb2ad0cbde1410856b3
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-05-09 13:15:24 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,6 +2,6 @@ import pytest
 import tornado.web
 from weatherapi import weather
 
-
-def test_case1(Williams%20Ag%20Airport):
-    assert weather("00AK") == "The Williams Ag Airport is a small_airport, which is belongs to Biggs. The current weather of the airport is clear sky, and the temperature is 299.33. The pressure is 1014 meanwhile the humidity is 31. In addition, the wind speed is 1.49 with 253 degrees."
+airport= 'Williams Ag Airport'
+def test_case1(airport):
+    assert weather(airport) == "The Williams Ag Airport is a small_airport, which is belongs to Biggs. The current weather of the airport is clear sky, and the temperature is 299.33. The pressure is 1014 meanwhile the humidity is 31. In addition, the wind speed is 1.49 with 253 degrees."
