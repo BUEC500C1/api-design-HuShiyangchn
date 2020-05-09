bdd8056fc671cc1d4b0bf525f5afc806a9560948
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-05-09 13:05:15 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,4 +1,7 @@
-def test_home_page(client):
-    rv = client.get('/')
-    assert rv.status_code == 200
-    assert rv.data == b'ok'
+import pytest
+import tornado.web
+from weatherapi import weather
+
+
+def test_case1(Williams Ag Airport):
+    assert weather("00AK") == "The Williams Ag Airport is a small_airport, which is belongs to Biggs. The current weather of the airport is clear sky, and the temperature is 299.33. The pressure is 1014 meanwhile the humidity is 31. In addition, the wind speed is 1.49 with 253 degrees."
