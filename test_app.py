72e8d4fef994300b45dff28797ba6f0affaa35c7
HuShiyangchn <55211567+HuShiyangchn@users.noreply.github.com>
2020-05-09 13:23:04 -0400

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -4,4 +4,4 @@ from weatherapi import weather
 
 airport= 'Williams Ag Airport'
 def test_case1():
-    assert weather(airport) == "The Williams Ag Airport is a small_airport, which is belongs to Biggs. The current weather of the airport is clear sky, and the temperature is 299.33. The pressure is 1014 meanwhile the humidity is 31. In addition, the wind speed is 1.49 with 253 degrees."
+    assert weather(airport) == 'The Williams Ag Airport is a small_airport, which is belongs to Biggs. The current weather of the airport is clear sky, and the temperature is 299.04.              The pressure is 1014 meanwhile the humidity is 31.              In addition, the wind speed is 1.49              with 253 degrees.'
